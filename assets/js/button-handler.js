d4c745afefbb22dd2737bf9933a1c5d0f4de68de
Niels Lange <info@nielslange.de>
2021-02-07 16:04:40 +0700

    🐛 FIX: Fix JS error

--- assets/js/button-handler.js
+++ assets/js/button-handler.js
@@ -13,12 +13,18 @@
 	var qty = document.querySelector( 'input.qty' );
 	var remove = document.querySelector( 'input.minus' );
 	var add = document.querySelector( 'input.plus' );
-	var min = document.querySelector( 'input.qty' ).getAttribute( 'min' );
-	var max = document.querySelector( 'input.qty' ).getAttribute( 'max' );
-	var step = document.querySelector( 'input.qty' ).getAttribute( 'step' );
+	if ( document.querySelector( 'input.qty' ) && document.querySelector( 'input.qty' ).hasAttribute( 'min' ) ) {
+		var min = document.querySelector( 'input.qty' ).getAttribute( 'min' );
+	}
+	if ( document.querySelector( 'input.qty' ) && document.querySelector( 'input.qty' ).hasAttribute( 'max' ) ) {
+		var max = document.querySelector( 'input.qty' ).getAttribute( 'max' );
+	}
+	if ( document.querySelector( 'input.qty' ) && document.querySelector( 'input.qty' ).hasAttribute( 'step' ) ) {
+		var step = document.querySelector( 'input.qty' ).getAttribute( 'step' );
+	}
 	var button = document.querySelector( 'button[name="update_cart"]' );
 
-	console.log( { qty, remove, add, min, max, step, button } );
+	// console.log( { qty, remove, add, min, max, step, button } );
 
 	// Handle update cart button.
 	function updateCartButton() {
@@ -29,25 +35,29 @@
 	}
 
 	// Decrease quantity.
-	remove.addEventListener( 'click', function( event ) {
-		if ( qty.value > parseInt( min ) ) {
-			qty.value = parseInt(qty.value) - parseInt(step);
-		}
-		updateCartButton();
-	}, false );
+	if ( remove ) {
+		remove.addEventListener( 'click', function( event ) {
+			if ( qty.value > parseInt( min ) ) {
+				qty.value = parseInt(qty.value) - parseInt(step);
+			}
+			updateCartButton();
+		}, false );
+	}
 
 	// Increase quantity.
-	add.addEventListener( 'click', function( event ) {
-		if ( max ) {
-			var temp = parseInt(qty.value) + parseInt(step);
-			if ( temp < parseInt( max ) ) {
-				qty.value = temp;
+	if ( add ) {
+		add.addEventListener( 'click', function( event ) {
+			if ( max ) {
+				var temp = parseInt(qty.value) + parseInt(step);
+				if ( temp < parseInt( max ) ) {
+					qty.value = temp;
+				}
+			} else {
+				qty.value = parseInt(qty.value) + parseInt(step);
 			}
-		} else {
-			qty.value = parseInt(qty.value) + parseInt(step);
-		}
-		updateCartButton();
-	}, false );
+			updateCartButton();
+		}, false );
+	}
 
 	// Call function after cart tottotals update.
 	jQuery( document.body ).on( 'updated_cart_totals', render );
